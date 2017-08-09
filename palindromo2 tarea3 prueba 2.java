a90ae38825792167f4d45674acbdd53b4bf19873
Parobacal <30785205+Parobacal@users.noreply.github.com>
2017-08-09 16:48:01 -0600

    Add files via upload

---
+++ palindromo2 tarea3 prueba 2.java
@@ -0,0 +1,27 @@
+package tarea3.prueba2;
+
+public class palindromo2 {
+    public boolean espalindromo(String cadena){
+    boolean valor=true;
+    int i,ind;    
+    String cadena2="";
+    
+    for (int x=0; x < cadena.length(); x++) {
+        if (cadena.charAt(x) != ' ')
+            cadena2 += cadena.charAt(x);
+    }
+    
+    cadena=cadena2;    
+    ind=cadena.length();
+    
+    for (i= 0 ;i < (cadena.length()); i++){        
+       if (cadena.substring(i, i+1).equals(cadena.substring(ind - 1, ind)) == false ) {
+          
+            valor=false;
+            break;
+       }
+       ind--;
+    }
+    return valor;
+}
+}
